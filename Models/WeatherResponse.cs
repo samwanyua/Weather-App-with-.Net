b0ad6633b36f59ff1d17ce7a905bdfd67fe2811f
samwanyua <samexample8@gmail.com>
2025-08-06 18:50:34 +0300

    Implement MVC structure

--- Models/WeatherResponse.cs
+++ Models/WeatherResponse.cs
@@ -17,6 +17,10 @@ namespace WeatherDashboard.Models
         public double Temp { get; set; }
         public double Feels_Like { get; set; }
         public int Humidity { get; set; }
+
+        public double Wind_Speed { get; set; }  
+        public int Clouds { get; set; }        
+
         public List<WeatherInfo> Weather { get; set; }
     }
 
--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews().AddNewtonsoftJson();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient(); // For WeatherController
+
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
